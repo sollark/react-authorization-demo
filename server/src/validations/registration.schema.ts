7e6389046912111d8fab6a88e02b40db92bf299f
Andrey Solonikov <andreysol@yahoo.com>
2023-04-12 15:36:13 +0300

    improve validation

--- server/src/validations/registration.schema.ts
+++ server/src/validations/registration.schema.ts
@@ -1,8 +1,16 @@
 import { body } from 'express-validator'
 
 export const registrationSchema = [
-  body('email').isEmail().withMessage('Email is not valid'),
+  body('email')
+    .exists()
+    .withMessage('Email is required')
+    .bail()
+    .isEmail()
+    .withMessage('Email is not valid'),
   body('password')
+    .exists()
+    .withMessage('Password is required')
+    .bail()
     .isLength({ min: 8, max: 24 })
     .withMessage('Password must be between 8 and 24 characters'),
 ]
