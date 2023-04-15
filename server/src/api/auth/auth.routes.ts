1f938d63663cbf1a8228a4d63a30ca177d2aa78d
Andrey Solonikov <andreysol@yahoo.com>
2023-04-15 15:58:49 +0300

    clean up

--- server/src/api/auth/auth.routes.ts
+++ server/src/api/auth/auth.routes.ts
@@ -4,7 +4,6 @@ import validateRequest from '../../middleware/validationHandler.js'
 import { refresh, registration, signIn, signOut } from './auth.controller.js'
 import { registrationSchema } from '../../validations/registration.schema.js'
 import { signInSchema } from '../../validations/signIn.schema.js'
-import { tokenSchema } from '../../validations/token.schema.js'
 
 const router = express.Router()
 
@@ -15,7 +14,8 @@ router.post(
   asyncHandler(registration)
 )
 router.post('/signin', signInSchema, validateRequest, asyncHandler(signIn))
-router.put('/signout', tokenSchema, asyncHandler(signOut))
-router.get('/refresh', tokenSchema, asyncHandler(refresh))
+router.put('/signout', asyncHandler(signOut))
+router.get('/refresh', asyncHandler(refresh))
+router.get('/account', asyncHandler(refresh))
 
 export { router as authRoutes }
