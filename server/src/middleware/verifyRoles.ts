8fc266c4fcc23bb621c719dd7ed194c78dd0c683
Andrey Solonikov <andreysol@yahoo.com>
2023-04-22 05:21:15 +0300

    verifyRoles middleware

---
+++ server/src/middleware/verifyRoles.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from 'express'
+import UnauthorizedError from '../errors/UnauthorizedError.js'
+import { userService } from '../api/user/user.service.js'
+
+async function verifyRoles(...allowedRoles: number[]) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const email = req.userData?.email
+
+      let hasAccess = false
+      if (!email) {
+        return next(
+          new UnauthorizedError(
+            'You are not authorized to access this resource'
+          )
+        )
+      }
+
+      const userRoles = await userService.getRoles(email)
+      if (!userRoles) {
+        return next(
+          new UnauthorizedError(
+            'You are not authorized to access this resource'
+          )
+        )
+      }
+
+      const roleCodes = Object.values(userRoles)
+
+      allowedRoles.forEach((role) => {
+        if (roleCodes.includes(role)) {
+          hasAccess = true
+        }
+      })
+
+      if (!hasAccess) {
+        return next(
+          new UnauthorizedError(
+            'You are not authorized to access this resource'
+          )
+        )
+      }
+
+      next()
+    } catch (error) {
+      return next(
+        new UnauthorizedError('You are not authorized to access this resource')
+      )
+    }
+  }
+}
