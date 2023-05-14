b96b6a8c151fa213d52d55912c624ca56bb3c410
Andrey Solonikov <andreysol@yahoo.com>
2023-05-14 10:49:04 +0300

    update input component

--- client/src/cmps/form/TextInput.tsx
+++ client/src/cmps/form/TextInput.tsx
@@ -1,7 +1,7 @@
 import { ErrorMessage } from '@hookform/error-message'
 import { TextField } from '@mui/material'
-import { ChangeEvent, FC } from 'react'
-import { useFormContext } from 'react-hook-form'
+import { FC } from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
 import { textInputStyle } from './formStyle'
 
 interface Props {
@@ -16,41 +16,33 @@ const Input: FC<Props> = (props: Props) => {
 
   //get methods from useFormContext
   const {
-    register,
-    setValue,
-    getValues,
     formState: { errors },
-    trigger,
+    control,
   } = useFormContext()
-
-  const formValues = getValues()
-
-  //update the value in the form and validate it
-  function onChange(e: ChangeEvent<HTMLInputElement>) {
-    setValue(name, e.target.value, { shouldValidate: false })
-    trigger(name)
-  }
 
   return (
     <>
-      <TextField
-        {...register(name)}
-        type={type}
-        label={label}
+      <Controller
+        control={control}
         name={name}
-        id={name}
-        placeholder={label}
-        value={formValues[name]}
-        onChange={onChange}
-        error={!!errors[name]}
-        helperText={
-          <ErrorMessage
-            name={name}
-            message={('* ' + errors[name]?.message) as string}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            type={type}
+            label={label}
+            id={name}
+            placeholder={label}
+            error={!!errors[name]}
+            helperText={
+              <ErrorMessage
+                name={name}
+                message={('* ' + errors[name]?.message) as string}
+              />
+            }
+            {...textInputStyle}
+            {...rest}
           />
-        }
-        {...textInputStyle}
-        {...rest}
+        )}
       />
     </>
   )
