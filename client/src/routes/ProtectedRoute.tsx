4a874e1fa4856e89e44fba15fb470158fe972634
Andrey Solonikov <andreysol@yahoo.com>
2023-05-17 12:44:38 +0300

    userRoles

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
+import { UserRole } from '@/config/userRoles'
 import { ReactNode } from 'react'
 
 interface ProtectedRouteProps {
   children: ReactNode
+  allowed: UserRole[]
 }
 
-const ProtectedRoute = ({ children }: ProtectedRouteProps): JSX.Element => {
-  console.log('ProtectedRoute')
+const ProtectedRoute = ({
+  children,
+  allowed,
+}: ProtectedRouteProps): JSX.Element => {
+  console.log('ProtectedRoute allowed', allowed)
   return <>{children}</>
 }
 
