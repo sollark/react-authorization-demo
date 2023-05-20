8476abe66c407a1113005e50943ac07854130957
Andrey Solonikov <andreysol@yahoo.com>
2023-05-20 13:36:48 +0300

    forms

--- client/src/cmps/Registration.tsx
+++ client/src/cmps/Registration.tsx
@@ -1,46 +1,15 @@
-import { RegistrationSchema } from '@/models/Auth'
-import { authService } from '@/service/auth.service'
 import { Box } from '@mui/material'
 import { Link } from '@tanstack/router'
 import { FC } from 'react'
-import Form from './form/Form'
 import RegistrationForm from './form/RegistrationForm'
-import SubmitButton from './form/buttons/SubmitButton'
-
-interface RegistrationForm {
-  email: string
-  password: string
-}
-
-const defaultValues = {
-  email: '',
-  password: '',
-  confirmedPassword: '',
-}
 
 const Registration: FC = () => {
   console.log('Registration connected')
 
-  async function submit(form: Object) {
-    console.log('Registration form submitted: ', form)
-
-    const { email, password } = form as RegistrationForm
-
-    const response = await authService.registration(email, password)
-
-    console.log('Registration response: ', response)
-  }
-
   return (
     <Box component='article' sx={{ maxWidth: '25rem', mx: 'auto', p: '1rem' }}>
       <h1>Registration</h1>
-      <Form
-        schema={RegistrationSchema}
-        defaultValues={defaultValues}
-        submit={submit}
-        submitButton={<SubmitButton />}>
-        <RegistrationForm />
-      </Form>
+      <RegistrationForm />
       <p>
         Already have an account? <Link to='/auth/signin'>Sign in</Link>
       </p>
--- client/src/cmps/form/RegistrationForm.tsx
+++ client/src/cmps/form/RegistrationForm.tsx
@@ -1,11 +1,37 @@
-import { FC } from 'react'
+import { RegistrationSchema } from '@/models/Auth'
+import { authService } from '@/service/auth.service'
+import Form from './Form'
+import SubmitButton from './buttons/SubmitButton'
 import Input from './inputs/TextInput'
 
-const RegistrationForm: FC = () => {
-  console.log('AccountForm connected')
+interface RegistrationForm {
+  email: string
+  password: string
+}
 
+const defaultValues = {
+  email: '',
+  password: '',
+  confirmedPassword: '',
+}
+
+const RegistrationForm = () => {
+  console.log('RegistrationForm connected')
+  async function submit(form: Object) {
+    console.log('Registration form submitted: ', form)
+
+    const { email, password } = form as RegistrationForm
+
+    const response = await authService.registration(email, password)
+
+    console.log('Registration response: ', response)
+  }
   return (
-    <>
+    <Form
+      schema={RegistrationSchema}
+      defaultValues={defaultValues}
+      submit={submit}
+      submitButton={<SubmitButton />}>
       <Input name='email' label='Email' type='email' />
       <Input name='password' label='Password' type='password' />
       <Input
@@ -13,7 +39,7 @@ const RegistrationForm: FC = () => {
         label={'Confirm password'}
         type='password'
       />
-    </>
+    </Form>
   )
 }
 
