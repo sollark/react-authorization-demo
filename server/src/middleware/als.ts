c6699aeef11651e0bddb524d0650e3b4f94f8369
Andrey Solonikov <andreysol@yahoo.com>
2023-06-03 16:35:13 +0300

    ais

--- server/src/middleware/als.ts
+++ server/src/middleware/als.ts
@@ -1,22 +1,26 @@
 import { NextFunction, Request, Response } from 'express'
 import { asyncLocalStorage } from '../service/als.service.js'
+import { tokenService } from '../service/token.service.js'
 
 async function setupAsyncLocalStorage(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  console.log('setupAsyncLocalStorage')
+  console.log('setupAsyncLocalStorage middleware')
 
   const storage = {}
   asyncLocalStorage.run(storage, async () => {
-    const userData = req.headers['X-User-Email'] as string
-    if (!userData) return next()
+    const refreshToken = req.cookies('refreshToken')
+    if (!refreshToken) return next()
+
+    const identifier = await tokenService.getIdentifier(refreshToken)
+    if (!identifier) return next()
 
     const alsStore = asyncLocalStorage.getStore()
     if (!alsStore) return next()
 
-    alsStore.userData = { identifier: userData }
+    alsStore.userData = { identifier }
 
     next()
   })
