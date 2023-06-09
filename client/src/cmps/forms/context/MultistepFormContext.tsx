03e258cd7d3b71cea5fd4dafa2b0621ab2ac614a
Andrey Solonikov <andreysol@yahoo.com>
2023-06-09 21:33:46 +0300

    multistepForm context

---
+++ client/src/cmps/forms/context/MultistepFormContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, ReactNode } from 'react'
+
+type MultistepFormContextProps = {
+  children: ReactNode
+  goToNextStep: () => void
+  goToPreviousStep: () => void
+  submitForm: () => void
+}
+
+type MultistepFormContextValue = {
+  children: ReactNode
+  goToNextStep: () => void
+  goToPreviousStep: () => void
+  submitForm: () => void
+}
+
+export const MultistepFormContext = createContext<
+  MultistepFormContextValue | undefined
+>(undefined)
+
+export const MultistepFormProvider = ({
+  children,
+  goToNextStep,
+  goToPreviousStep,
+  submitForm,
+}: MultistepFormContextProps) => {
+  const contextValue: MultistepFormContextValue = {
+    children,
+    goToNextStep,
+    goToPreviousStep,
+    submitForm,
+  }
+
+  return (
+    <MultistepFormContext.Provider value={contextValue}>
+      {children}
+    </MultistepFormContext.Provider>
+  )
+}
