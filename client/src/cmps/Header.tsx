32d7102d6d5c6bfc2bdd91d2191fb45f93add47c
Andrey Solonikov <andreysol@yahoo.com>
2023-06-22 17:45:52 +0300

    header and navigation styles

--- client/src/cmps/Header.tsx
+++ client/src/cmps/Header.tsx
@@ -1,27 +1,23 @@
 import { FC } from 'react'
 import Logo from './Logo'
-import useAccountStore from '@/stores/accountStore'
-import useUserStore from '@/stores/userStore'
 import Navigation from './Navigation'
+import { Box } from '@mui/material'
 
 const Header: FC = () => {
   console.log('Header connected')
 
-  const { isComplete } = useAccountStore()
-  const { user } = useUserStore()
-
   return (
     <header className='app-header'>
-      <Logo />
-      <Navigation />
-
-      <div className='app-header__user'>
-        {user ? user.firstName : 'Not signed in'}
-      </div>
-
-      <div className='app-header__account-status'>
-        {isComplete ? 'Complete' : 'Your account is not complete'}
-      </div>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: '100%',
+        }}>
+        <Logo />
+        <Navigation />
+      </Box>
     </header>
   )
 }
--- client/src/cmps/Navigation.tsx
+++ client/src/cmps/Navigation.tsx
@@ -1,11 +1,19 @@
+import { Box } from '@mui/material'
 import { Link } from '@tanstack/router'
 
 const Navigation = () => {
   return (
-    <div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: '1rem',
+        height: '100%',
+      }}>
       <Link to='/'>Home</Link>
       <Link to='/shift'>Shift</Link>
-    </div>
+    </Box>
   )
 }
 
