76f64745053044f91ea91997678568d1e6d142ca
Andrey Solonikov <andreysol@yahoo.com>
2023-06-27 15:52:09 +0300

    fixes

--- client/src/service/store.service.ts
+++ client/src/service/store.service.ts
@@ -1,18 +1,15 @@
-import useAccountStore from '@/stores/accountStore'
 import useOrganizationStore from '@/stores/orgainzaionStore'
 import useRoleStore from '@/stores/roleStore'
 import useUserStore from '@/stores/userStore'
 
 function clearStoreStates() {
   const clearUser = useUserStore((state) => state.clearUser)
-  const clearAccount = useAccountStore((state) => state.clearAccount)
   const clearRoles = useRoleStore((state) => state.clearRoles)
   const clearOrganization = useOrganizationStore(
     (state) => state.clearOrganization
   )
 
   clearUser()
-  clearAccount()
   clearOrganization()
   clearRoles()
 }
