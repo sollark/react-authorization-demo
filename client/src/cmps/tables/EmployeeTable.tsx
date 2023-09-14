98fe98317a9d3ca8d80c6563fd40fdc03e000db7
Andrey Solonikov <andreysol@yahoo.com>
2023-09-14 14:54:27 +0300

    add table component and employee table

---
+++ client/src/cmps/tables/EmployeeTable.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+import employeeData from '../../assets/mock_data/employee.json'
+import Table from './Table'
+
+export type Employee = {
+  firstName: string
+  lastName: string
+  company: string
+  department: string
+  position: string
+  role: string
+  status: string
+}
+
+const employeeColumns = [
+  { field: 'firstName', headerName: 'First name' },
+  { field: 'lastName', headerName: 'Last name' },
+  { field: 'company', headerName: 'Company' },
+  { field: 'department', headerName: 'Department' },
+  { field: 'position', headerName: 'Position' },
+  { field: 'role', headerName: 'Role' },
+  { field: 'status', headerName: 'Status' },
+]
+
+const employeeActionColumn = [{ field: 'action', headerName: 'Action' }]
+
+const EmployeeTable: FC = () => {
+  const columns = [...employeeColumns, ...employeeActionColumn]
+  return (
+    <div>
+      <h2>Employee Table</h2>
+      <Table dataRows={employeeData} tableColumns={columns} />
+    </div>
+  )
+}
+
+export default EmployeeTable
