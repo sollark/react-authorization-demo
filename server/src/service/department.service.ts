5d0ab616588a41875fc1dd61e86016c1f1a717c4
Andrey Solonikov <andreysol@yahoo.com>
2023-10-15 15:12:59 +0300

    department service

--- server/src/service/department.service.ts
+++ server/src/service/department.service.ts
@@ -1,3 +1,5 @@
+import { Types } from 'mongoose'
+import BadRequestError from '../errors/BadRequestError.js'
 import DepartmentModel from '../mongodb/models/department.model.js'
 import logger from './logger.service.js'
 
@@ -12,6 +14,21 @@ async function createDepartment(departmentName: string) {
   return newDepartment
 }
 
+async function getDepartmentDBId(
+  departmentName: string
+): Promise<Types.ObjectId> {
+  const department = await DepartmentModel.findOne({ departmentName })
+  if (!department) {
+    logger.warn(
+      `departmentService - department is not found: ${departmentName}`
+    )
+    throw new BadRequestError('Department is not found')
+  }
+
+  return department._id
+}
+
 export const departmentService = {
   createDepartment,
+  getDepartmentDBId,
 }
