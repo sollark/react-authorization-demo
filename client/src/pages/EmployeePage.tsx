b051579f57a05b0a0e9785147e5043a00933fe84
Andrey Solonikov <andreysol@yahoo.com>
2023-11-09 18:15:13 +0200

    shoe different employee table based on user role

--- client/src/pages/EmployeePage.tsx
+++ client/src/pages/EmployeePage.tsx
@@ -1,11 +1,26 @@
 import AdvancedEmployeeTable from '@/cmps/tables/emloyeeTable/AdvancedEmployeeTable'
+import BasicEmployeeTable from '@/cmps/tables/emloyeeTable/BasicEmployeeTable'
+import EditableEmployeeTable from '@/cmps/tables/emloyeeTable/EditableEmployeeTable'
+import { Role } from '@/models/Role'
 import { employeeService } from '@/service/employee.service'
+import useRoleStore from '@/stores/roleStore'
 import { Box } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import { FC } from 'react'
 
+const TableViews = {
+  Guest: BasicEmployeeTable,
+  User: BasicEmployeeTable,
+  Supervisor: AdvancedEmployeeTable,
+  Manager: EditableEmployeeTable,
+  Admin: EditableEmployeeTable,
+}
+
 const EmployeePage: FC = () => {
   console.log(' Employee connected')
+
+  const role: Role = useRoleStore((state) => state.role)
+  const Table = TableViews[role] || BasicEmployeeTable
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: ['company'],
@@ -32,7 +47,7 @@ const EmployeePage: FC = () => {
       <h1>Employee page</h1>
       {/* {JSON.stringify(data)} */}
       {data.companyName}
-      <AdvancedEmployeeTable
+      <Table
         employees={data.employees}
         departmentOptions={
           data.departments.map((department) => department.departmentName) || []
