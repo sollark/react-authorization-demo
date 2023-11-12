587f3070fe116cbd45a34bb611e6a0f7e058232d
Andrey Solonikov <andreysol@yahoo.com>
2023-11-12 15:51:53 +0200

    add company service

---
+++ client/src/service/company.service.ts
@@ -0,0 +1,25 @@
+import { Company } from '@/models/Company'
+import { httpService } from './axios/http.service'
+
+async function getBasicCompanyData(): Promise<Company | null> {
+  const response = await httpService.get<null, Company>('company', null)
+
+  console.log('companyService - getBasicCompanyData, response.data', response)
+
+  const { success, message, data } = response as any
+  if (!success) {
+    console.log(message)
+    return null
+  }
+
+  const company = data.company as Company
+
+  return company ? company : null
+}
+
+async function getAdvancedCompanyData() {}
+
+export const companyService = {
+  getBasicCompanyData,
+  getAdvancedCompanyData,
+}
