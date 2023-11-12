9880a06c0611b666d592cd6c7169354c07c5a2df
Andrey Solonikov <andreysol@yahoo.com>
2023-11-12 15:48:51 +0200

    add accessForbiddenPage

---
+++ client/src/pages/AccessForbiddenPage.tsx
@@ -0,0 +1,24 @@
+import { Box } from '@mui/material'
+import { FC } from 'react'
+
+const AccessForbiddenPage: FC = () => {
+  console.log('AccessForbidden connected')
+
+  return (
+    <Box
+      sx={{
+        m: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        maxHeight: '100vh',
+        maxWidth: ['100%', '100%', '100%', '75%', '75%'],
+        backgroundColor: 'primary.light',
+      }}>
+      <h1>You have no permission</h1>
+    </Box>
+  )
+}
+
+export default AccessForbiddenPage
