b0c5a57d76ef77ae0d5749c0db4fedb5147341bc
Andrey Solonikov <andreysol@yahoo.com>
2023-11-22 12:00:03 +0200

    add persist to role

--- client/src/stores/roleStore.ts
+++ client/src/stores/roleStore.ts
@@ -1,6 +1,6 @@
 import { Role, USER_ROLE } from '@/models/Account'
 import { create } from 'zustand'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
 import { zustandLogger } from './zustandLogger'
 
@@ -13,12 +13,15 @@ type RoleState = {
 // Create a store with initial state
 const useRoleStore = create<RoleState>()(
   zustandLogger(
-    devtools(
-      immer((set) => ({
-        role: USER_ROLE.guest,
-        setRole: (role) => set(() => ({ role })),
-        clearRole: () => set(() => ({ role: USER_ROLE.guest })),
-      }))
+    persist(
+      devtools(
+        immer((set) => ({
+          role: USER_ROLE.guest,
+          setRole: (role) => set(() => ({ role })),
+          clearRole: () => set(() => ({ role: USER_ROLE.guest })),
+        }))
+      ),
+      { name: 'role-storage' }
     )
   )
 )
