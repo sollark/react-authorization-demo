268a8dbbd66d681b50ea72a415df8d29e18e4f8a
Andrey Solonikov <andreysol@yahoo.com>
2024-01-06 18:34:41 +0200

    Refactor profile.service.ts to handle API responses

--- client/src/service/profile.service.ts
+++ client/src/service/profile.service.ts
@@ -1,22 +1,44 @@
 import { Profile } from '../models/Profile'
-import { httpService } from './axios/http.service'
+import {
+  ApiErrorResponse,
+  ApiSuccessResponse,
+  httpService,
+} from './axios/http.service'
 import { log } from './console.service'
 
 type ProfileData = {
   newProfile: Profile
 }
 
+// TODO Get a blank profile from server
 async function createBlankProfile(profile: Profile): Promise<Profile | null> {
-  const data = await httpService.post<Profile, ProfileData>(
+  const response = await httpService.post<Profile, ProfileData>(
     '/createBlankProfile',
     profile
   )
 
-  log('profileService - createBlankProfile, data', data)
+  const data = handleResponse(response, 'profileService - createBlankProfile')
 
   return data?.newProfile || null
 }
 
 export const profileService = {
   createBlankProfile,
+}
+
+function handleResponse<T>(
+  response: ApiSuccessResponse<T> | ApiErrorResponse | null,
+  functionName: string
+): T | null {
+  if (!response) {
+    log(`${functionName}, no response from the server`)
+    return null
+  }
+
+  if (!response.success) {
+    log(`${functionName}, message`, response.message)
+    return null
+  }
+
+  return response.data || null
 }
