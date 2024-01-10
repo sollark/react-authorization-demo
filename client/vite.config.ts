8606f5340ea7e572d6a619b06768ae723260a536
Andrey Solonikov <andreysol@yahoo.com>
2024-01-10 07:13:00 +0200

    Update Vite and server configurations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,14 +5,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // vite app server
   server: {
     port: 3000,
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..'],
-    },
   },
-  publicDir: '../public',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,6 +6,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development'
 const SERVER_PORT = process.env.PORT || 3030
 
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS || 'http://localhost:3000'
+const PROXY = process.env.VITE_SERVER_PROXY_URL
 
 const MONGO_URL = process.env.MONGO_URL
 
@@ -20,6 +21,7 @@ export const config = {
   server: {
     port: SERVER_PORT,
     origins: ALLOWED_ORIGINS.split(','),
+    proxy: PROXY,
   },
   jwt: {
     accessSecret: JWT_ACCESS_SECRET,
