c5a9cbc481271323bb2af327876bd3367b422aec
Andrey Solonikov <andreysol@yahoo.com>
2024-01-11 07:52:55 +0200

    build 0.5

--- server/dist/config/config.js
+++ server/dist/config/config.js
@@ -3,6 +3,7 @@ dotenv.config();
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const SERVER_PORT = process.env.PORT || 3030;
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS || 'http://localhost:3000';
+const PROXY = process.env.VITE_SERVER_PROXY_URL;
 const MONGO_URL = process.env.MONGO_URL;
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
@@ -14,6 +15,7 @@ export const config = {
     server: {
         port: SERVER_PORT,
         origins: ALLOWED_ORIGINS.split(','),
+        proxy: PROXY,
     },
     jwt: {
         accessSecret: JWT_ACCESS_SECRET,
--- server/dist/server.js
+++ server/dist/server.js
@@ -4,51 +4,60 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { accountRoutes } from './api/account/account.routes.js';
+import { authRoutes } from './api/auth/auth.routes.js';
+import { companyRoutes } from './api/company/company.routes.js';
+import { employeeRoutes } from './api/employee/employee.routes.js';
+import { profileRoutes } from './api/profile/profile.routes.js';
 import { config } from './config/config.js';
-import { connectMongo } from './mongodb/connect.js';
-// import middleware
 import setupAsyncLocalStorage from './middleware/als.js';
 import { deleteSensitiveData } from './middleware/deleteSensitiveData.js';
 import errorHandler from './middleware/errorHandler.js';
-// import routes
-import { accountRoutes } from './api/account/account.routes.js';
-import { authRoutes } from './api/auth/auth.routes.js';
-import { employeeRoutes } from './api/employee/employee.routes.js';
-// import for __dirname
-import { fileURLToPath } from 'url';
-import { companyRoutes } from './api/company/company.routes.js';
-import { profileRoutes } from './api/profile/profile.routes.js';
+import { connectMongo } from './mongodb/connect.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 // CORS
-if (config.env === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')));
-}
-else {
+if (config.env === 'development') {
     app.use(cors({
         credentials: true,
         origin: config.server.origins,
     }));
 }
-// middlewares
+// Middlewares
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
-// async local storage
-app.all('*', setupAsyncLocalStorage);
-// delete sensitive data ('__v', '_id', 'identifier', 'password', 'uuid')
-app.use(deleteSensitiveData);
-// routes
+app.all('*', setupAsyncLocalStorage); // async local storage
+app.use(deleteSensitiveData); // delete sensitive data ('__v', '_id', 'identifier', 'password', 'uuid')
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/account', accountRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/employee', employeeRoutes);
 app.use('/api/company', companyRoutes);
-// server globals
-const publicPath = path.join(__dirname, 'public', 'index.html');
+// Serve i18n files
+app.use('/i18n', express.static(path.join(__dirname, '../public/i18n')));
+// Proxy middleware for development environment:
+// Redirects all requests to the Vite development server to enable hot-reloading and other dev features.
+if (config.env === 'development') {
+    app.use('/', createProxyMiddleware({
+        target: config.server.proxy,
+        changeOrigin: true,
+        ws: true, // proxy websockets
+    }));
+}
+// Serve static files for production
+if (config.env === 'production') {
+    const staticPath = path.join(__dirname, '../public');
+    app.use(express.static(staticPath));
+}
+// Serve index.html for all other routes
+const publicPath = path.join(__dirname, '../public', 'index.html');
 const clientRoute = '/**';
 app.get(clientRoute, (req, res, next) => res.sendFile(publicPath));
 // 404
@@ -56,11 +65,11 @@ app.use(clientRoute, (req, res, next) => {
     const error = new Error(`${req.method} ${req.originalUrl} not found!`);
     next(error);
 });
-// error handler
+// Error handler
 app.use(errorHandler);
-// start server
+// Start server
 server.listen(config.server.port, () => console.log(`Server is up and running on port ${config.server.port}`));
-// connect to MongoDB
+// Connect to MongoDB
 try {
     await connectMongo();
 }
