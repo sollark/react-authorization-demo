781393c6f43532a83646649ffa434ffff9214e68
Andrey Solonikov <andreysol@yahoo.com>
2024-01-12 20:33:54 +0200

    clean code

--- client/src/models/response/AccountResponse.ts
+++
@@ -1,9 +0,0 @@
-import { Account } from '../Account'
-
-export interface AccountResponse {
-  account: Account
-}
-
-export function isAccountResponse(obj: any): obj is AccountResponse {
-  return obj && typeof obj === 'object' && 'account' in obj
-}
--- client/src/models/response/AuthResponse.ts
+++
@@ -1,7 +0,0 @@
-export type AuthResponse = {
-  accessToken: string
-}
-
-export function isAuthResponse(obj: any): obj is AuthResponse {
-  return obj && typeof obj === 'object' && 'accessToken' in obj
-}
