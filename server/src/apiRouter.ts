7e76cc185372dce1e4985d1b43995f0ec9ad9b96
Andrey Solonikov <andreysol@yahoo.com>
2024-01-18 19:01:12 +0200

    build 0.81

--- server/src/apiRouter.ts
+++ server/src/apiRouter.ts
@@ -1,14 +1,12 @@
-// apiRouter.ts
-import express from 'express'
-
 import cookieParser from 'cookie-parser'
-import { accountRoutes } from './api/account/account.routes'
-import { authRoutes } from './api/auth/auth.routes'
-import { companyRoutes } from './api/company/company.routes'
-import { employeeRoutes } from './api/employee/employee.routes'
-import { profileRoutes } from './api/profile/profile.routes'
-import setupAsyncLocalStorage from './middleware/als'
-import { deleteSensitiveData } from './middleware/deleteSensitiveData'
+import express from 'express'
+import { accountRoutes } from './api/account/account.routes.js'
+import { authRoutes } from './api/auth/auth.routes.js'
+import { companyRoutes } from './api/company/company.routes.js'
+import { employeeRoutes } from './api/employee/employee.routes.js'
+import { profileRoutes } from './api/profile/profile.routes.js'
+import setupAsyncLocalStorage from './middleware/als.js'
+import { deleteSensitiveData } from './middleware/deleteSensitiveData.js'
 
 const router = express.Router()
 
