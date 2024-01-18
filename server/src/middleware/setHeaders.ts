fd387f1c392e07d3fc7211b5b4c6777a1709b640
Andrey Solonikov <andreysol@yahoo.com>
2024-01-18 20:17:33 +0200

    build 0.85

--- server/src/middleware/setHeaders.ts
+++ server/src/middleware/setHeaders.ts
@@ -8,7 +8,7 @@ function setHeaders(req: Request, res: Response, next: NextFunction) {
   res.setHeader('X-Content-Type-Options', 'nosniff')
   res.setHeader('X-Frame-Options', 'DENY')
   res.setHeader('X-XSS-Protection', '1; mode=block')
-  res.setHeader('Content-Security-Policy', "default-src 'self'")
+  //res.setHeader('Content-Security-Policy', "default-src 'self'") //render blocks static files with this header
   next()
 }
 
