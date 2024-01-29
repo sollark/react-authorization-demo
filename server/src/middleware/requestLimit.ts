abbb905c6658d943f8726338114049a3a38a69d2
Andrey Solonikov <andreysol@yahoo.com>
2024-01-29 14:39:36 +0200

    Update request limit configuration

--- server/src/middleware/requestLimit.ts
+++ server/src/middleware/requestLimit.ts
@@ -6,8 +6,8 @@ type RequestRecord = {
 }
 
 const rateLimit = {
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 500, // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes',
   requests: {} as Record<string, RequestRecord>,
 }
